3a189a0f7fe744e6dab2c96d55947a645d782663
S Choi <66569284+anhourandaquarter@users.noreply.github.com>
2021-08-11 23:26:21 -0700

    added some methods and doc'ing
    top_five: returns the five most frequently-occurring words in a tweet. If there are ties, it should go by whichever ones appear first.
    count_hashtags: returns a set of all hashtags in the tweet and a count of how many there are
    count_tags: same as count_hashtags but counts tags instead

--- reptweet.py
+++ reptweet.py
@@ -1,11 +1,8 @@
 # proper doc'ing tba
 # flake8 pass tbd
 
-# testing here! -Nikki
 
 # imports here
-#   scraper tba?
-#   scikitlearn?
 import pandas as pd
 import re
 from textblob import TextBlob
@@ -16,11 +13,15 @@ from sklearn.tree import DecisionTreeClassifier
 # ALL_CAPS_CONSTANT = pd.read_csv(data.csv)
 # ALL_CAPS_CONSTANT.head() to check the columns
 
-# cast the tweet column into TextBlobs
+# Tweet column to be cast as TextBlobs. 
+# All functions that use 'tweet' as a parameter assume that the tweet is a TextBlob.
 
 
 # functions here
 def normalize(term):
+    """
+    Normalizes a single word by changing it to lowercase and removing all punctuation. 
+    """
     term = term.lower()
     term = re.sub(r'\W+', '', term)
 
@@ -30,7 +31,9 @@ def normalize(term):
 
 
 def most_freq_word_per_tweet(tweet):
-    # consider stopwords?
+    """
+    Takes in a tweet and returns the most frequently occurring word in the tweet
+    """
     uniques = (tweet.words)
     highest_freq = 0
     most_freq_word = ''
@@ -40,6 +43,41 @@ def most_freq_word_per_tweet(tweet):
             highest_freq = freq
             most_freq_word = word
     return most_freq_word
+
+
+def top_five(tweet):
+    """
+    Takes in a tweet and returns the five most-occurring words in that tweet.
+    """
+    words = {}
+    uniques = (tweet.words)
+    for word in uniques:
+        words[word] = tweet.words.count(word)
+    sorted_words = dict(sorted(words.items(), key=lambda item: item[1]))
+    return sorted_words.keys()[0:4]
+
+
+def count_hashtags(tweet):
+    """
+    Takes in a tweet and returns the set of unique hashtags and how many hashtags there are in the tweet.
+    """
+    hashtags = set()
+    for word in tweet.words:
+        if word[0] == '#':
+            hashtags.add(word)
+    return hashtags, len(hashtags)
+
+
+def count_tags(tweet):
+    """
+    Takes in a tweet and returns the set of unique tags and how many tags there are in the tweet.
+    """
+    tags = set()
+    for word in tweet.words:
+        if word[0] == '@':
+            tags.add(word)
+    return tags, len(tags)
+
 
 
 # differentiate hashtags and tag-tags
