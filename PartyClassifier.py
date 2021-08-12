99d06fcb1b174bc0d1e902405ddad859616e50c8
nikkifalicov <88803948+nikkifalicov@users.noreply.github.com>
2021-08-12 16:43:02 -0700

    Update PartyClassifier.py

--- PartyClassifier.py
+++ PartyClassifier.py
@@ -1,9 +1,9 @@
 """
 Nikki Falicov
 CSE 163 AB
-This class takes in scraped TweetCongress data, and generates a machine learning
-model to try and predict political party based on the various labels present in
-the scraped data
+This class takes in scraped TweetCongress data in CSV format, and generates a machine
+learning model to try and predict political party based on the various labels present
+in the dataframe
 """
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -20,6 +20,11 @@ contents (approximate):
 COMMENTS ARE PRELIM
 """
 class PartyClassifier:
+    """
+    Uses machine learning to classify tweets from different political representatives
+    during the month of July 2021. Contains functionality to compare different train/test
+    split ratios and their outcomes, as well as the mean squared error of the model.
+    """
     
     def __init__(self, data):
         """
@@ -31,16 +36,17 @@ class PartyClassifier:
     
     def get_column_names(self):
         """
-        returns column names within dataframe
+        get_column_name returns the column names within the data of the current
+        PartyClassifier object
         """
         return self._data.columns
     
     def compare_outcomes(self, features, labels):
         """
-        takes in the list of features and labels, and returns a list of tuples
-        detailing the mean squared error for different splits ranging
-        from 0.1 to 0.9 inclusive. returned list is sorted from least
-        to greatest by mean squared error.
+        compare_outcomes(features, labels) takes in a list of features and labels
+        for the current PartyClassifier objects' dataframe, and returns a list of
+        tuples in format (mean squared error, split). This list is sorted from least
+        to greatest based on mean squared error.
         """
         # make list of the various test sizes from 0.1 to 0.9
         test_sizes = [x/10 for x in range(1, 10)]
@@ -63,8 +69,11 @@ class PartyClassifier:
 
     def fit_and_predict_party(self, test_data_size = 0.2):
         """
-        creates a machine learning model, with the training/test split
-        detailed in parameter, returning the mean squared error
+        fit_and_predict_party(test_data_size) takes in a test data size
+        (from 0 to 1 inclusive), and returns the mean squared error
+        of the machine learning model fitted and predicted on that 
+        train/test split. The default value for test data size is 0.2,
+        signifying a 80%/20% training/testing data split.
         """
         filtered = self._data.loc[:, ['state', 'text', 'party']]
         filtered = filtered.dropna()
@@ -82,6 +91,14 @@ class PartyClassifier:
 
 
     def _model_with_names(self, test_data_size = 0.2):
+        """
+        _model_with_names(test_data_size) is a private method designed to
+        test the inclusion of names as a feature. takes in a test data size
+        (from 0 to 1 inclusive), and returns the mean squared error
+        of the machine learning model fitted and predicted on that 
+        train/test split. The default value for test data size is 0.2,
+        signifying a 80%/20% training/testing data split.
+        """
         filtered = self._data.loc[:, ['state', 'text', 'party', 'name']]
         filtered = filtered.dropna()
         features = filtered.loc[:, filtered.columns != 'party']
