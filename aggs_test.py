eabdc3820c689e5477cd106b972ac9f3b3f0e739
S Choi <66569284+anhourandaquarter@users.noreply.github.com>
2021-08-18 00:17:28 -0700

    done with this too :D

--- aggs_test.py
+++ aggs_test.py
@@ -2,49 +2,102 @@
 Suh Young Choi
 CSE 163 AB
 
-aggs_test: a tester file for the functions in the Aggregator class
+This file allows a user to view the functions which will be
+later used in visuals.py, and to check that their output is
+correct. This file uses a scaled-down version of the full
+TweetCongress dataset for testing purposes.
+
+Necessary imports:
+- Aggregator
 """
-
 from aggs import Aggregator
-# from cse163_utils import assert_equals
-
-DATA = '/home/test.csv'
 
 
-def test_display_functions(agg):
-    print("Democrat hashtags: " + str(agg.dem_hashtags))
-    print("Republican hashtags: " + str(agg.repub_hashtags))
-    print("All hashtags: " + str(agg.hashtags))
+DATA = 'tweet_test2'
 
+
+def test_hashtag_functions(agg):
+    """
+    Prints all fields from the Aggregator class relating to hashtags
+    """
+    print("Democrat hashtags:")
+    print(agg.dem_hashtags)
+    print("Republican hashtags:")
+    print(agg.rep_hashtags)
+    print("All hashtags:")
+    print(agg.hashtags)
     print()
-
-    print("Democrat tags: " + str(agg.dem_tags))
-    print("Republican tags: " + str(agg.repub_tags))
-    print("All tags: " + str(agg.tags))
-
+    print("Top 10 Democrat hashtags:")
+    print(agg.dem_top_ten_hashtags)
+    print("Top 5 Democrat hashtags:")
+    print(agg.dem_top_five_hashtags)
     print()
+    print("Top 10 Republican hashtags:")
+    print(agg.dem_top_ten_hashtags)
+    print("Top 5 Republican hashtags:")
+    print(agg.dem_top_five_hashtags)
 
-    print("Top ten words across the board: " + str(agg.top_ten_words))
-    print("Top five words across the board: " + str(agg.top_five_words))
 
+def test_tag_functions(agg):
+    """
+    Prints all fields from the Aggregator class relating to tags.
+    """
+    print("Democrat tags:")
+    print(agg.dem_tags)
+    print("Republican tags:")
+    print(agg.rep_tags)
+    print("All tags:")
+    print(agg.tags)
     print()
-
-    print("Top ten Democratic words: " + str(agg.dem_top_ten))
-    print("Top five Democratic words: " + str(agg.dem_top_five))
-
+    print("Top 10 Democrat tags:")
+    print(agg.dem_top_ten_tags)
+    print("Top 5 Democrat tags:")
+    print(agg.dem_top_five_tags)
     print()
+    print("Top 10 Republican tags:")
+    print(agg.dem_top_ten_tags)
+    print("Top 5 Republican tags:")
+    print(agg.dem_top_five_tags)
 
-    print("Top ten Republican words: " + str(agg.repub_top_ten))
-    print("Top five Republican words: " + str(agg.repub_top_five))
 
+def test_top10s_functions(agg):
+    """
+    Prints all fields from the Aggregator class relating to words.
+    """
+    print("Top ten words across the board:")
+    print(agg.top_ten_words)
+    print("Top five words across the board:")
+    print(agg.top_five_words)
     print()
-    print("Display functions complete.")
+    print("Top ten Democratic words:")
+    print(agg.dem_top_ten_words)
+    print("Top five Democratic words:")
+    print(agg.dem_top_five_words)
+    print()
+    print("Top ten Republican words: ")
+    print(agg.rep_top_ten_words)
+    print("Top five Republican words: ")
+    print(agg.rep_top_five_words)
 
 
 def main():
     agg = Aggregator(DATA)
     print("Aggregator successfully initialized.")
-    test_display_functions(agg)
+    print()
+
+    test_hashtag_functions(agg)
+    print("***HASHTAG TESTING COMPLETE***")
+    print()
+
+    test_tag_functions(agg)
+    print("***TAG TESTING COMPLETE***")
+    print()
+
+    test_top10s_functions(agg)
+    print("***TOP 10S TESTING COMPLETE***")
+    print()
+    print()
+    print("You're good to go!")
 
 
 if __name__ == '__main__':
